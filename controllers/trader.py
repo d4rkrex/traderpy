190d680ca855fb0bf06500b313f668772e01bf86
manuel <d4rkrex@LAPTOP-0UTF1SFJ.localdomain>
2022-06-14 19:46:56 -0300

    add futures function with ccxt

--- controllers/trader.py
+++ controllers/trader.py
@@ -1,4 +1,4 @@
-import os
+import os, ccxt
 import json
 from flask import request
 from flask import Blueprint
@@ -9,6 +9,7 @@ from utils.logger import Logger
 bp = Blueprint("trader", __name__)
 WEBHOOK_PASSPHRASE = os.environ['WEBHOOK_PASSPHRASE']
 log = Logger()
+ticker_dict = {"BTCUSDT":"BTC/USDT", "GMTUSDT":"GMT/USDT", "":"" }
 
 @bp.route("/", methods=['POST'])
 def webhook():
@@ -17,14 +18,17 @@ def webhook():
     if data['passphrase'] == WEBHOOK_PASSPHRASE:
         ticker = data['ticker']
         side = data['strategy']['order_action'].upper()
-        quantity = data['strategy']['order_contracts']
+        quantity = str(data['strategy']['order_contracts'])
+        price = data['bar']['close']
         
         if data['client'] == '1':
-            client = Client(os.environ['API_KEY_ONE'], os.environ['API_SECRET_ONE'])
+            #client = Client(os.environ['API_KEY_ONE'], os.environ['API_SECRET_ONE'])
+            client = client_one()
         else:
             client = Client(os.environ['API_KEY_TWO'], os.environ['API_SECRET_TWO'])
 
-        order_response = order(client, side, quantity, ticker)
+        order_response = future_order(client, side, quantity, ticker, price)
+        #order_response = order(client, side, quantity, ticker)
         log.info(order_response)
         if order_response:
             return buildResponse(200, 'success')
@@ -54,3 +58,34 @@ def buildResponse(statusCode, body=None):
     if body is not None:
         response['body'] = json.dumps(body)
     return response
+
+def client_one():
+    exchange_class = getattr(ccxt, 'binance')
+    exchange = exchange_class({
+            'apiKey': os.environ['API_KEY_ONE'],
+            'secret': os.environ['API_SECRET_ONE'],
+            'timeout': 10000,
+            'enableRateLimit': True,
+            'rateLimit': 250,
+            'options': {
+                'defaultType': 'future',
+            }
+        })
+    return exchange
+
+def future_order(exchange, side, quantity, symbol, price, order_type=ORDER_TYPE_MARKET):
+    #exchange_class = getattr(ccxt, 'binance')
+    #exchange.verbose = True
+    try:
+        params = {
+            'newClientOrderId': "{}-{}".format(price, side),
+            'activationPrice': price,  
+            'callbackRate': '2',
+            'workingType': 'CONTRACT_PRICE',
+            'reduceOnly': 'true',
+        }
+        exchange.createOrder(symbol, 'TRAILING_STOP_MARKET', side, quantity, price, params)
+    except Exception as e:
+        log.error(f"an exception occured - {e}")
+        return False
+    return order
