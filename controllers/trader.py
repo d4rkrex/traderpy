f5f18c75209902ea55781901f7b6bb84a2740537
manuel <d4rkrex@LAPTOP-0UTF1SFJ.localdomain>
2022-08-21 00:12:49 -0300

    adding multy buy - minor fixes

--- controllers/trader.py
+++ controllers/trader.py
@@ -38,6 +38,7 @@ def webhook():
         if ticker == "total2":
             for t in total2:
                 if order_approval(client, side, t):
+                    log.info(t)
                     order_response = order(client, side, total2[t], t)
                     log.info(order_response)
                 else: order_response = False
